518d5602c58c42bb877ec3fc434c0ea2f4b00869
FJCon <conilfernando@gmail.com>
2025-05-27 14:39:34 -0300

    template

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+
+import { template } from "./src/settings";
+
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [react(), tailwind(), sitemap()],
+    site: 'lucaspetruccelli.github.io',
+    base: 'curriculum'
+,
+});
